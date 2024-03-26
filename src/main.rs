c848d0a6b06112a500bb269a589c872f9cf63225
kaushik raj <algorithmunloack@gmail.com>
2024-03-26 22:13:47 +0530

    This is the basic of asyn fn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
- # [macro_use ]extern crate rocket ;
-#[get("/")]
+#![allow(dead_code ,unused_variables)]
+use std::future::Future;
 
- fn hi () -> & 'static str {
-    "Welcom"
+ fn main (){
+   println!("Hello world laila ");
+   let x = foo1();
  }
-  #[launch]
-fn rocket ()-> _ {
- rocket ::build().mount( "/", routes![hi])
-}
+ async fn foo1 ()-> usize {
+   println!("hiuo");
+   0
+ }
+  fn foo2 () -> impl Future<Output = usize >{
+   async {
+      println!("foooo");
+      0
+   }
+  }
