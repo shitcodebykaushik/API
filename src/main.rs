3d8dd949ac97ded1d764e60658aa781ba9d1410a
kaushik raj <algorithmunloack@gmail.com>
2024-03-27 19:20:29 +0530

    OK

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-#![allow(dead_code ,unused_variables)]
-use std::future::Future;
-
- fn main (){
-   println!("Hello world laila ");
-   let x = foo1();
- }
- async fn foo1 ()-> usize {
-   println!("hiuo");
-   0
- }
-  fn foo2 () -> impl Future<Output = usize >{
-   async {
-      println!("foooo");
-      0
-   }
-  }
+fn main (){
+  
+}
